9c27ad2f3581c730f952db1b1870f5bd8ffbd537
Oscar Javier Urrutia Gutierrez <oscarj.ugutierrez@gmail.com>
2022-12-13 02:14:37 -0500

    Homework 1: Duck

--- dino_runner/components/dinosaur.py
+++ dino_runner/components/dinosaur.py
@@ -1,10 +1,11 @@
 import pygame
 from pygame.sprite import Sprite
-from dino_runner.utils.constants import RUNNING, JUMPING
+from dino_runner.utils.constants import RUNNING, JUMPING, DUCKING
 
 
 RUNNING_ACTION = 'running'
 JUMPING_ACTION = 'jumping'
+DUCKING_ACTION = 'ducking'
 
 class Dinosaur(Sprite):
     X_POS = 80
@@ -25,19 +26,24 @@ class Dinosaur(Sprite):
             self.run()
         elif self.action == JUMPING_ACTION:
             self.jump()
+        elif self.action == DUCKING_ACTION:
+            self.duck()
 
         if self.action != JUMPING_ACTION:
             if user_input[pygame.K_UP]:
                 self.action = JUMPING_ACTION
+            elif user_input[pygame.K_DOWN]:
+                self.action = DUCKING_ACTION
             else:
                 self.action = RUNNING_ACTION
-
-
+            
         if self.step >= 9:
             self.step = 0
 
     def run(self):
         self.image = RUNNING[0] if self.step < 5 else RUNNING[1]
+        self.rect.x = self.X_POS
+        self.rect.y = self.Y_POS
         self.step += 1
 
     def jump(self):
@@ -51,7 +57,11 @@ class Dinosaur(Sprite):
             self.action = RUNNING_ACTION
         
     def duck(self):
-        pass
+        self.image = DUCKING[0] if self.step < 5 else DUCKING[1]
+        self.rect = self.image.get_rect()
+        self.rect.x = self.X_POS
+        self.rect.y += self.Y_POS + 35
+        self.step += 1
 
     def draw(self, screen):
         screen.blit(self.image, (self.rect.x, self.rect.y))
